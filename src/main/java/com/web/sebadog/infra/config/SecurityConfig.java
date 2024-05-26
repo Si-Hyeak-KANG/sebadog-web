80d59e8b71a68ea2b6db361b36d0b42b570db4f4
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-05-26 14:09:48 +0900

    cors, csrf disable 설정

--- src/main/java/com/web/sebadog/infra/config/SecurityConfig.java
+++ src/main/java/com/web/sebadog/infra/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,6 +28,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         log.info("-------------Security Config-----------");
         http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/view/sign-up", "/", "/account/**").permitAll()
                         .anyRequest().authenticated())
