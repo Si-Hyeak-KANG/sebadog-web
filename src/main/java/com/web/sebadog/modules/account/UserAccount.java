7bcdf8a878b927aaec869592b526a1eaf6ad3ecc
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-05-28 12:32:32 +0900

    로그인한 계정 프로필 토글 박스 구현

--- src/main/java/com/web/sebadog/modules/account/UserAccount.java
+++ src/main/java/com/web/sebadog/modules/account/UserAccount.java
@@ -10,7 +10,7 @@ public class UserAccount extends User {
     private final Account account;
 
     public UserAccount(Account account) {
-        super(account.getEmail(), account.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        super(account.getNickname(), account.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_USER")));
         this.account = account;
     }
 
